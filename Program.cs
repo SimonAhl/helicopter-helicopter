0b2118719595003115872c278ecb12afba08d9ff
SimonAhl <siah04001@utb.vaxjo.se>
2021-11-12 14:08:11 +0100

    hleicpoptre

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ghk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string helicopter = Console.ReadLine();
+
+            char [] Helicopter = helicopter.ToCharArray();
+
+            Array.Reverse(Helicopter);
+
+            string HELICOPTER = new string (Helicopter);
+
+            if(helicopter.Equals(HELICOPTER)){
+                Console.WriteLine("helicopter");
+            }else{
+                Console.WriteLine("HELICOPTER");
+            }
+        }
+    }
+}
